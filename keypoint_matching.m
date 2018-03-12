5ae96fc311a0f026e3eec7c5caa517f013ce88c8
Nils Hulzebosch <macnilsmac@hotmail.com>
2018-03-12 16:25:17 +0100

    draw lines between matching points

--- keypoint_matching.m
+++ keypoint_matching.m
@@ -1,20 +1,24 @@
-function [ keypoint_matchings ] = keypoint_matching(image1, image2)
-% git clone git://github.com/vlfeat/vlfeat.git
-%run('vlfeat-0.9.21-bin/vlfeat-0.9.21/toolbox/vl_setup')
-%run('/Users/Nils/vlfeat-0.9.21/toolbox/vl_setup')
-
+function [ keypoint_matchings ] = keypoint_matching(image1, image2, visualizePoints)
 %KEYPOINT_MATCHING Find the keypoint matchings between two images.
 % Input arguments:
-%   image1, image2     Rgb or grayscale images.
-% Dependencies:
-%   VLFeat (see http://www.vlfeat.org/install-matlab.html)
+%   image1, image2      Rgb or grayscale images.
+%   visualizePoints     Amount of features (points) to display (default: 50).
+
+% DEPENDENCIES:
+%%%   VLFeat (see http://www.vlfeat.org/install-matlab.html)
+%%% git clone git://github.com/vlfeat/vlfeat.git
+%%%run('vlfeat-0.9.21-bin/vlfeat-0.9.21/toolbox/vl_setup')
+%%%run('/Users/Nils/vlfeat-0.9.21/toolbox/vl_setup')
 
 close ALL % close all figures
 
-% set default images to 'boat'
+% set default parameters
 if nargin == 0
     image1 = imread('boat1.pgm');
     image2 = imread('boat2.pgm');
+end
+if nargin < 3
+    visualizePoints = 50;
 end
 
 % transform to grayscale if necessary
@@ -34,42 +38,42 @@ s_image2 = single(image2);
 [f1, d1] = vl_sift(s_image1);
 [f2, d2] = vl_sift(s_image2);
 
-%%%%%%%
-% show 50 randomly selected features and their descriptors on top of image1
-%perm = randperm(size(f1, 2));
-%sel = perm(1:50);
-%h1 = vl_plotframe(f1(:, sel));
-%h2 = vl_plotframe(f1(:, sel));
-%set(h1, 'color', 'k', 'linewidth', 3);
-%set(h2, 'color', 'y', 'linewidth', 2);
-
-%h3 = vl_plotsiftdescriptor(d1(:, sel), f1(:, sel));
-%set(h3, 'color', 'g');
-% code hierboven puur voor visualisatie, kan later weg
-%%%%%%%
-
 % find matching features
 [matches, ~] = vl_ubcmatch(d1, d2);
 
 
+%%% Visualization %%%
 % Take a random subset (with set size set to 50) of all matching points, 
 % and plot on the image. Connect matching pairs with lines.
-figure, imshow(image1), title('Image 1 descriptors')
-perm = randperm(size(matches, 2));
-sel_ = perm(1:50);
-sel = matches(1, sel_);
-h1 = vl_plotframe(f1(:, sel));
-h2 = vl_plotframe(f1(:, sel));
+
+figure, imshowpair(image1, image2, 'montage') % init figure
+title('Matching features in both images')
+
+perm = randperm(size(matches, 2)); % shuffle indices randomly
+sel = perm(1:visualizePoints); % pick the first 50 (default) 
+
+% Draw the interest poins for image1
+sel1 = matches(1, sel);
+h1 = vl_plotframe(f1(:, sel1));
+h2 = vl_plotframe(f1(:, sel1));
 set(h1, 'color', 'k', 'linewidth', 3);
 set(h2, 'color', 'y', 'linewidth', 2);
 
-figure, imshow(image2), title('Image 2 descriptrs')
-sel = matches(2, sel_);
-h1 = vl_plotframe(f2(:, sel));
-h2 = vl_plotframe(f2(:, sel));
+% Draw the interest poins for image2
+sel2 = matches(2, sel);
+f2(1, :) = f2(1, :) + size(image1, 2); % 850 pixels to the right, because image2 is next to image1
+h1 = vl_plotframe(f2(:, sel2));
+h2 = vl_plotframe(f2(:, sel2));
 set(h1, 'color', 'k', 'linewidth', 3);
 set(h2, 'color', 'y', 'linewidth', 2);
 
-%TODO: turn into one figure and draw lines between matching points
+hold on
+
+% Draw lines between each pair of points
+for i = 1:50 
+    x = [f1(1, sel1(i)) f2(1, sel2(i))];
+    y = [f1(2, sel1(i)) f2(2, sel2(i))];
+    line(x, y, 'Color', 'cyan', 'LineWidth', 1)
+end
 
 end
